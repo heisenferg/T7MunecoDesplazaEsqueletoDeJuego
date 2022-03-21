f614ddf8ef500b6d3748f407fb3b49430c426e15
Fernando Fernández <heisenferg@gmail.com>
2022-03-21 20:41:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/esqueletodejuego/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.esqueletodejuego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Juego(this));
+    }
+}
